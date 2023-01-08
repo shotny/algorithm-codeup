c5951887483ada8b4d852baf865b9d7c6e0333ea
shotny <yeahh2483@gmail.com>
2023-01-08 23:18:14 +0900

    23.01.08 Baekjoon

---
+++ src/main/java/baekjoon/steps/step7/_8_10757.java
@@ -0,0 +1,14 @@
+package baekjoon.steps.step7;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class _8_10757 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+
+        System.out.println(a.add(b));
+    }
+}
