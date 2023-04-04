f435ac4754507b05459ec853420c763782199978
shotny <yeahh2483@gmail.com>
2023-04-04 17:24:52 +0900

    23.04.04

---
+++ src/main/java/programmers/lv1/_1_추억점수.java
@@ -0,0 +1,32 @@
+package programmers.lv1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1_추억점수 {
+    public static void main(String[] args) {
+
+    }
+
+    public class solution{
+        public int[] solution(String[] name, int[] yearning, String[][] photo) {
+            int[] answer = new int[photo.length];
+
+            Map<String, Integer> point = new HashMap<>();
+            for (int i = 0; i < name.length; i++) {
+                point.put(name[i], yearning[i]);
+            }
+
+            for (int i = 0; i < photo.length; i++) {
+                int sum = 0;
+                for (int j = 0; j < photo[i].length; j++) {
+                    if(point.containsKey(photo[i][j])){
+                        sum += point.get(photo[i][j]);
+                    }
+                }
+                answer[i] = sum;
+            }
+            return answer;
+        }
+    }
+}
