1906aa2e5e8bd446b18404a74bfc9aa9845b1809
shotny <yeahh2483@gmail.com>
2023-05-21 23:49:51 +0900

    23.05.21.

---
+++ src/main/java/inf_kim/section2_Array/Main10.java
@@ -0,0 +1,40 @@
+package inf_kim.section2_Array;
+
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] board = new int[n+2][n+2];
+
+        // 가장자리 0으로 초기화
+        for (int i = 0; i < n + 2; i++) {
+            board[0][i] = 0;
+            board[n+1][i] = 0;
+            board[i][0] = 0;
+            board[i][n+1] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                board[i][j] = scanner.nextInt();
+            }
+        }
+        scanner.close();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                board[i][j] = scanner.nextInt();
+            }
+        }
+    }
+}
+// 상하좌우 네 번 비교
+// 가장 자리 값은 0
+// board[i][j] 일때,
+// if(i==0){'상'은 비교 안함}
+// if(i==n-1){'하'는 비교 안함}
+// if(j==0){'좌'비교안함}
+// if(j==n-1){'우' 비교 안함}
+// x -- 각 격자판의 자연수가 0 이상이라는 조건 없음~~ :)
